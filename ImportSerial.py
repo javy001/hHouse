822c905d600b512618b14048ba92f4f3739657e7
Javier Quintero <javierq@TruJavy.local>
2017-07-06 21:06:35 -0700

    add db connection

--- ImportSerial.py
+++ ImportSerial.py
@@ -2,10 +2,17 @@ import serial
 import datetime as dt
 from sqlalchemy import create_engine
 
+con = create_engine('mysql+pymysql://javierq:abcd1234@ec2-54-68-139-60.us-west-2.compute.amazonaws.com')
+
 arduino = serial.Serial('/dev/ttyACM0')
 arduino.baudrate = 9600
 
-while True:
+i = 0
+while i<21:
     data = arduino.readline()
     pieces = data.split(',')
     print '{0}: {1}'.format(pieces,dt.datetime.now())
+    if len(pieces) == 2:
+        sql = "insert into read_port.temp1 (date_key,temp) values({0},'{1}')".format(pieces,dt.datetime.now())
+        con.connect().execute(sql)
+        i += 1
